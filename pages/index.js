1ccb72a3023bf0a98b04c1261cf3721928e9cc47
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2022-08-22 21:45:16 +0700

    next-check-it

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,33 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      {/* Import font here! */}
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      {/* Introduction about websites */}
       <div className={styles.container}>
-        {/* <Link href="/create_class">
-          <img src="./Img/Image_NK/NK17.jpg" className={styles.image} />
-        </Link> */}
-        <div>
-          <form className={styles.form_ctn}>
-            <label for="Name">Name :</label>
-            <input type="text" id="name" name="Name" />
-            <label for="Lastname">Lastname :</label>
-            <input type="text" id="lastname" lastname="lastname" />
-            <button type="submit">Send</button>
-          </form>
+        <div className={styles.intro_ctn}>
+          <div className={styles.title}>
+            <h1 className={styles.font}>Introduction</h1>
+          </div>
+          <div className={styles.subtitle}>
+            <h2 className={styles.font}>What is Check-IT ?</h2>
+          </div>
+          <div>
+            <p className={styles.font}>
+              Check-IT is a website for recording attendance of students of
+              Pathumganga School. This website was created by students of
+              Pathumganga School to solve environmental problems. due to Using a
+              large amount of paper from the use of paper for check names.
+            </p>
+          </div>
         </div>
-        
       </div>
     </div>
   );
