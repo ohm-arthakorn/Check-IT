02f2a52e6479b18d3d5226e2153b381c660bbc11
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2022-08-27 11:33:56 +0700

    navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -5,25 +5,27 @@ export default function Navbar() {
   return (
     <div>
       {/* สร้าง Navigation Bar ที่เป็น Responsive Design */}
-      <div className="max-w-6xl mx-auto px-4 my-8">
-        {/* Logo in Container Navbar */}
+      <div className="max-w-6xl min-w-3xl p-2 mx-auto px-4 my-2 bg-blue-400 rounded-lg">
+        
         <div className="flex justify-between">
+          {/* Logo in Container Navbar */}
           <span className="flex justify-center">
             <Link href="/" className="">
               <img src="/IMG/LG01.png" alt="Logo" className="w-24" />
             </Link>
           </span>
           {/* Create Class & Checking Result  in Container Navbar */}
+
           {/* Button Create Class */}
           <span  className="flex justify-center">
             <Link href="/create_class">
-              <button>Create Class</button>
+              <button className="text-white">Create Class</button>
             </Link>
           </span>
           {/* Button Checking Result */}
           <span className="flex justify-center">
             <Link href="/checking_result">
-              <button>Checking Result</button>
+              <button className="text-white">Checking Result</button>
             </Link>
           </span>
         </div>
