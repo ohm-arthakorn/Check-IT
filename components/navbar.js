06fa2fa37064830add9974238392c01fef4b765e
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2022-08-27 11:24:32 +0700

    next-check-it

--- components/navbar.js
+++ components/navbar.js
@@ -1,53 +1,33 @@
+import React from "react";
 import Link from "next/link";
-import styles from "../styles/navbar.module.css";
-import Head from "next/head";
+
 export default function Navbar() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap"
-          rel="stylesheet"
-        />
-        <title>Check-It</title>
-      </Head>
-      {/* Logo Linking Home */}
-      <Link href="/">
-        <img src="./Img/LG01.png" alt="logo" className={styles.logo} />
-      </Link>
-
-      {/* Quote on Navbar */}
-      <span className={styles.text}>
-        <h4 className={styles.font}>Check your class.</h4>
-        <h5 className={styles.font}>Check-IT</h5>
-      </span>
-
-      {/* navigation on create class */}
-      <div className={styles.button_ctn}>
-        <Link href="/create_class">
-          <span>
-            <button type="button" className={styles.button}>
-              Create Class Pages
-            </button>
+    <div>
+      {/* สร้าง Navigation Bar ที่เป็น Responsive Design */}
+      <div className="max-w-6xl mx-auto px-4 my-8">
+        {/* Logo in Container Navbar */}
+        <div className="flex justify-between">
+          <span className="flex justify-center">
+            <Link href="/" className="">
+              <img src="/IMG/LG01.png" alt="Logo" className="w-24" />
+            </Link>
           </span>
-        </Link>
-        <Link href="/Made_by">
-          <span>
-            <button type="button" className={styles.button_2}>
-              Made By
-            </button>
+          {/* Create Class & Checking Result  in Container Navbar */}
+          {/* Button Create Class */}
+          <span  className="flex justify-center">
+            <Link href="/create_class">
+              <button>Create Class</button>
+            </Link>
           </span>
-        </Link>
-        <Link href="/test">
-          <span>
-            <button type="button" className={styles.button_2}>
-              Testing
-            </button>
+          {/* Button Checking Result */}
+          <span className="flex justify-center">
+            <Link href="/checking_result">
+              <button>Checking Result</button>
+            </Link>
           </span>
-        </Link>
+        </div>
       </div>
-      </div>
+    </div>
   );
 }
--- pages/create_class.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-import React from 'react';
-
-export default function CreateClass() {
-
-    return (
-        <div></div>
-    );
-};
