e17c6090e58d837506d26b6c6111206347671fad
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2022-09-12 08:11:06 +0700

    next-check-it

--- pages/classApp.js
+++ pages/classApp.js
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Head from "next/head"
-import { data } from "autoprefixer";
+import Head from "next/head";
 
 export default function classApp() {
   return (
     <div>
       <Head>
-        <title>
-          รายชื่อนักเรียนชั้นม.5/1
-        </title>
+        <title>รายชื่อนักเรียนชั้นม.5/1</title>
       </Head>
       <div className="flex flex-col items-center my-4">
-      <Link href="/create_form">
-      <button className="border border-black p-2 rounded font-serif hover:p-4 hover:bg-blue-200 duration-500">เพิ่มรายชื่อนักเรียนที่นี่ !!</button>
-      </Link>
-      </div>
-      <div>
-        
+        <Link href="/create_form">
+          <button className="border border-black p-2 rounded font-serif hover:p-4 hover:bg-blue-200 duration-500">
+            เพิ่มรายชื่อนักเรียนที่นี่ !!
+          </button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
